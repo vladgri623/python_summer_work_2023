8c780efd70073cd15d5f3a10a38150598a7b865a
walrus-burial <vladgri623@gmail.com>
2023-04-26 21:27:11 +0300

    Initial commit

---
+++ test1.py
@@ -0,0 +1,47 @@
+# print('Hello world!')
+#
+# s = int(input('-->'))
+#
+# print(s)
+# print(f"Hello, {s}")
+#
+# print(s, type(s))
+# print(s*2)
+
+
+# s = input("What's your name?")
+# print("Hello!", '\n', s)
+#
+# print(f"Hello, {s}")
+# x = int(input("How old are you?"))
+# print(f"I'm {x}")
+
+
+# # 1
+# f_name = str(input("What's your first name?"))
+# s_name = str(input("What's your second name?"))
+#
+# print(f"Hello, {f_name} {s_name}")
+
+
+# # 2
+# name1, name2, name3 = str(input()), str(input()), str(input())
+# print(f"{name3}:{name2}:{name1}")
+
+
+# import this
+# print(this)
+
+
+# import keyword
+# print(*keyword.kwlist)
+# print(keyword.kwlist)
+
+
+x = int(input())
+if x > 0:
+    print('Positive')
+elif x == 0:
+    print('Zero')
+else:
+    print('Negative')
